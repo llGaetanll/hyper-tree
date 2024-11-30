e8b48a047e7a8e69b9ccbfc292817910a75de972
Gaetan Almela <gaetan.almela@gmail.com>
2024-11-29 21:45:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# `hyper-tree`
+
+A zero-cost generalization over quadtrees and octrees that store points.
+
+- No dependecies!
+- 100% safe code
+- Small footprint
+
+This is **not** a [kd-tree](https://en.wikipedia.org/wiki/K-d_tree)! While
+kd-trees are also used for binary space partitioning, their splits are not
+necessarily equal, as they are in quadtrees, octrees, and more general forms.
+
+## What can I do with this?
+
+Right now, not much. This is still very much a work in progress. 
+
+## Work in Progress!
+
+The library needs to be tested and benched.
+
+- [ ] Write tests
+- [ ] Benchmark
+- [ ] Allow creating padded trees
+      Right now, new points can't be added to a tree without having to
+      completely rebuild it.
---
+++ src/traits.rs
@@ -0,0 +1,84 @@
+pub trait Mean: Copy {
+    fn mean(self, other: Self) -> Self;
+}
+
+pub trait Epsilon {
+    /// The smallest non-zero value of the type
+    const EPSILON: Self;
+}
+
+impl Mean for u8 {
+    fn mean(self, other: Self) -> Self {
+        ((self as u32) + (other as u32) / 2u32) as Self
+    }
+}
+
+impl Mean for u16 {
+    fn mean(self, other: Self) -> Self {
+        ((self as u32) + (other as u32) / 2u32) as Self
+    }
+}
+
+impl Mean for u32 {
+    fn mean(self, other: Self) -> Self {
+        ((self as u64) + (other as u64) / 2u64) as Self
+    }
+}
+
+impl Mean for u64 {
+    fn mean(self, other: Self) -> Self {
+        ((self as u128) + (other as u128) / 2u128) as Self
+    }
+}
+
+impl Mean for i8 {
+    fn mean(self, other: Self) -> Self {
+        ((self as i32) + (other as i32) / 2i32) as Self
+    }
+}
+
+impl Mean for i16 {
+    fn mean(self, other: Self) -> Self {
+        ((self as i32) + (other as i32) / 2i32) as Self
+    }
+}
+
+impl Mean for i32 {
+    fn mean(self, other: Self) -> Self {
+        ((self as i64) + (other as i64) / 2i64) as Self
+    }
+}
+
+impl Mean for i64 {
+    fn mean(self, other: Self) -> Self {
+        ((self as i128) + (other as i128) / 2i128) as Self
+    }
+}
+
+macro_rules! impl_epsilon_int {
+    ($t:ty) => {
+        impl Epsilon for $t {
+            const EPSILON: Self = 1 as $t;
+        }
+    };
+
+    ($t:ty, $($ts:ty),+) => {
+        impl_epsilon_int!($t);
+
+        impl_epsilon_int!($($ts),+);
+    };
+}
+
+#[rustfmt::skip]
+impl_epsilon_int!(
+    i8, i16, i32, i64, i128,
+    u8, u16, u32, u64, u128
+);
+
+impl Epsilon for f32 {
+    const EPSILON: Self = Self::EPSILON;
+}
+
+impl Epsilon for f64 {
+    const EPSILON: Self = Self::EPSILON;
+}
